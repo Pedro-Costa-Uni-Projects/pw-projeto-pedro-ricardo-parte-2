4fbe427ca44d758310eff16f335518a02be90a38
Pedro Costa <a21904825@alunos.ulht.pt>
2021-05-18 10:05:43 +0100

    base e index/home feitos

---
+++ website/urls.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+from django.urls import path
+from . import views
+
+app_name = "website"
+
+urlpatterns = [
+    path('', views.home_page_view, name='home'),
+    path('home', views.home_page_view, name='home'),
+    path('benificios', views.benificios_page_view, name='benificios'),
+    path('about', views.about_page_view, name='about'),
+    path('comentarios', views.comentarios_page_view, name='comentarios'),
+    path('estilos', views.estilos_page_view, name='estilos'),
+    path('galeria', views.galeria_page_view, name='galeria'),
+    path('provas', views.provas_page_view, name='provas'),
+]
--- website/views.py
+++ website/views.py
@@ -1,3 +1,31 @@
 from django.shortcuts import render
 
+
 # Create your views here.
+
+def home_page_view(request):
+    return render(request, 'website/index.html')
+
+
+def about_page_view(request):
+    return render(request, 'website/about.html')
+
+
+def benificios_page_view(request):
+    return render(request, 'website/benificios.html')
+
+
+def comentarios_page_view(request):
+    return render(request, 'website/comentarios.html')
+
+
+def estilos_page_view(request):
+    return render(request, 'website/estilos.html')
+
+
+def galeria_page_view(request):
+    return render(request, 'website/galeria.html')
+
+
+def provas_page_view(request):
+    return render(request, 'website/provas.html')
