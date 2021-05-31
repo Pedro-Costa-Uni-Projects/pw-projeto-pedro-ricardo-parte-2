08b4b45c7f5a2acbf472b4cd97139418fe50eac6
a21904553git config --global user.name a21904553git config --global user.name a21904553 <a21904553@alunos.ulht.pt>
2021-05-31 09:52:59 +0100

    deasd

--- website/models.py
+++ website/models.py
@@ -2,12 +2,21 @@ from django.db import models
 
 
 # Create your models here.
-class Contacto(models.Model):
+class Pessoa(models.Model):
+    id = models.AutoField(primary_key=True)
     nome = models.CharField(max_length=40)
     apelido = models.CharField(max_length=40)
+
+    def __str__(self):
+        return self.nome + " " + self.apelido
+
+
+class Contacto(models.Model):
+    pessoaid = models.ForeignKey(Pessoa)
     email = models.EmailField()
     telefone = models.CharField(max_length=17, blank=True)
     dataNascimento = models.DateField(verbose_name="Data de Nascimento")
+
 
     def __str__(self):
         return self.nome + " " + self.apelido
@@ -15,21 +24,13 @@ class Contacto(models.Model):
 
 class Comentario(models.Model):
     id = models.AutoField(primary_key=True)
-
     clareza = models.CharField(max_length=10, default=3)
-
     rigor = models.CharField(max_length=10, default=3)
-
     precisao = models.CharField(max_length=10, default=3)
-
     optimizacao = models.BooleanField(verbose_name="Boa optimização para telemóvel", default=False)
-
     tempoResposta = models.BooleanField(verbose_name="Tempo de resposta do Website", default=False)
-
     facilUsar = models.BooleanField(verbose_name="Fácil de Usar", default=False)
-
     facilLer = models.BooleanField(verbose_name="Fácil de Ler", default=False)
-
     feedBack = models.TextField(default="")
 
     def __str__(self):
@@ -39,8 +40,7 @@ class Comentario(models.Model):
 class Quizz(models.Model):
     id = models.AutoField(primary_key=True)
     pontos = models.IntegerField(default=0)
-    nome = models.CharField(max_length=40)
-    apelido = models.CharField(max_length=40)
+    pessoaid = models.ForeignKey(Pessoa)
     p1 = models.IntegerField(default=0)
     p2 = models.CharField(max_length=40, default="")
     p3 = models.CharField(max_length=40, default="")
